9aeb5c83e87095fa18b1c6c30b0ddbaa105cfef7
Bohdan Pylypchenko <bogdan314ylypchenko@gmail.com>
2023-05-13 19:54:58 +0300

    Add main source

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from waitress import serve
+from loguru import logger
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    port = 80
+    logger.info(f"Starting app on port {port}")
+    serve(app, port=port)
