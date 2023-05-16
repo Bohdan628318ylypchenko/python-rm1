5ba1ea47c0eb81b9fad4e940ef2d2d0a5c86ad35
Bohdan Pylypchenko <bogdan314ylypchenko@gmail.com>
2023-05-16 14:54:42 +0300

    Fixing errors, adding root, e1, e2

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ from loguru import logger
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello_world_root():
+    return 'Hello World! (from root)'
+
+
 @app.route('/e1')
-def hello_world():
+def hello_world_1():
     return 'Hello World! (from endpoint 1)'
 
 
 @app.route('/e2')
-def hello_world():
+def hello_world_2():
     return 'Hello World! (from endpoint 2)'
 
 
